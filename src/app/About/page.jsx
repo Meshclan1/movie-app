7a4ff3d439a6b626389b93c5bf083a35d6d1c48d
Ayo <Ayo.meshe@gmail.com>
2024-03-25 01:11:33 +0000

    update the about page

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -2,13 +2,27 @@ import React from "react";
 
 export default function AboutPage() {
   return (
-    <div>
-      <h1>About Page</h1>
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About Page</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
-        illum a porro mollitia fugiat enim adipisci, atque assumenda nemo
-        blanditiis, qui voluptatem deserunt eos provident ipsam consequuntur
-        excepturi unde fuga?
+        Welcome to the Movie Database where we're passionate about all things
+        film. Whether you're a casual moviegoer, a dedicated cinephile, or a
+        budding filmmaker, our platform is your go-to destination for
+        discovering, exploring, and celebrating the world of cinema.
+      </p>
+      <p>
+        With a vast collection of movies spanning genres, eras, and cultures, we
+        offer a comprehensive resource for finding your next favorite film. From
+        timeless classics to the latest blockbusters, our database is
+        meticulously curated to provide you with an unparalleled movie-watching
+        experience.
+      </p>
+      <p>
+        So whether you're seeking cinematic inspiration, conducting research for
+        your next movie night, or simply indulging your love of film, we invite
+        you to dive into the Movie Database and embark on a cinematic journey
+        like no other. Welcome to the Movie Database – where the magic of movies
+        comes to life!
       </p>
     </div>
   );
