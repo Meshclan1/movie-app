644a64555d9e959b93636f755a56b391605238b7
Ayo <Ayo.meshe@gmail.com>
2024-03-25 11:22:48 +0000

    update navbar component

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import NavBarItem from "./NavBarItem";
 
-export default function NavBar({ title, params }) {
+export default function NavBar() {
   return (
-    <div>
-      <NavBarItem title="Trending" params="fetchTrending" />
-      <NavBarItem title="Top Rated" params="fetchTopRated" />
+    <div className="flex dark:bg-gray-600 bg-amber-100 p-4 lg-text-lg justify-center gap-6">
+      <NavBarItem title="Trending" param="fetchTrending" />
+      <NavBarItem title="Top Rated" param="fetchTopRated" />
     </div>
   );
 }
