ff398ab2c14e5d45811ce2a72741e5a68a1f19cd
Ayo <Ayo.meshe@gmail.com>
2024-03-25 17:58:59 +0000

    minor fix on error page

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -7,7 +7,6 @@ export default function Error({ error, reset }) {
   useEffect(() => {
     console.log(error);
   }, [error]);
-
   return (
     <div className="text-center mt-10">
       <h1>Something went wrong.. Please try again later.</h1>
